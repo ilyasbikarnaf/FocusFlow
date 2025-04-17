5b5a14e84a9255dee23952fa8fa86d40eb0416ab
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2025-04-17 17:47:12 +0100

    finish signup page basic design

---
+++ lib/schemas/auth.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const SignupSchema = z
+  .object({
+    email: z.string().email(),
+    password: z
+      .string()
+      .regex(new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"), {
+        message:
+          "Password must be at least 8 characters and contain an uppercase letter, lowercase letter, and number",
+      }),
+    confirmPassword: z.string(),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+  });
+
+export const SigninSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
