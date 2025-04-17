fb4c0dd2d4d05f49505e0133947790d7f1eaae4b
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2025-04-17 18:36:11 +0100

    create user schema

---
+++ db/schema.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v: string) {
+          return /.+\@.+\..+/.test(v);
+        },
+        message: (props: { value: any }) =>
+          `${props.value} is not a valid email!`,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      validate: {
+        validator: function (v: string) {
+          return /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/.test(v);
+        },
+        message:
+          "Password must contain at least one uppercase letter, one number, and one special character.",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", UserSchema);
