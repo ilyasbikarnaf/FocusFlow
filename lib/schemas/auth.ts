7448e6ea1d219feaaf0bef5cc3f565453e002719
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2025-04-18 16:41:27 +0100

    add checking for space in zod schemas

--- lib/schemas/auth.ts
+++ lib/schemas/auth.ts
@@ -2,13 +2,23 @@ import { z } from "zod";
 
 export const SignupSchema = z
   .object({
-    email: z.string().email(),
+    email: z
+      .string()
+      .email()
+      .refine((val) => !val.includes(" "), {
+        message: "Email must not contain spaces",
+      }),
+
     password: z
       .string()
       .regex(new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$"), {
         message:
           "Password must be at least 8 characters and contain an uppercase letter, lowercase letter, and number",
+      })
+      .refine((val) => !val.includes(" "), {
+        message: "Password must not contain spaces",
       }),
+
     confirmPassword: z.string(),
   })
   .refine(({ password, confirmPassword }) => password === confirmPassword, {
@@ -17,8 +27,17 @@ export const SignupSchema = z
   });
 
 export const SigninSchema = z.object({
-  email: z.string().email(),
+  email: z
+    .string()
+    .email()
+    .refine((value) => !value.includes(" "), {
+      message: "Email must not contain spaces",
+    }),
+
   password: z
     .string()
-    .min(8, { message: "Password must be at least 8 characters" }),
+    .min(8, { message: "Password must be at least 8 characters" })
+    .refine((val) => !val.includes(" "), {
+      message: "Password must not contain spaces",
+    }),
 });
